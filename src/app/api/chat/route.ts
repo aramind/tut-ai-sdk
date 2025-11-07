796fa7ffe9692e12ea825a36a9244a8ebbee6201
aramind <88814622+aramind@users.noreply.github.com>
2025-11-07 17:46:34 +0800

    feat: add chat feature

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,3 +1,17 @@
+import { ai } from "@/app/constants/ai";
+import { UIMessage, streamText, convertToModelMessages } from "ai";
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  try {
+    const { messages }: { messages: UIMessage[] } = await req.json();
+
+    const result = streamText({
+      model: ai.model,
+      messages: convertToModelMessages(messages),
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.error("Error streaming chat completion:", error);
+    return new Response("Failed to stream chat completion", { status: 500 });
+  }
 }
